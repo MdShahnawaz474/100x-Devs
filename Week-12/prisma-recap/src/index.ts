89d347731d40775b63398ffb523cdaddf7add3b3
MD Shahnawaz <mdshahnawazm17@gmail.com>
2024-10-18 03:47:26 +0530

    .

--- Week-12/prisma-recap/src/index.ts
+++ Week-12/prisma-recap/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(username : string, password :string, firstName:string, lastName:string,email:string){
+    const response =await prisma.user.create({
+        data:{
+            username,
+            password,
+            firstName,
+            lastName,
+            email
+        }
+    })
+    console.log(response);
+    
+}
+
+insertUser("fayyazkhan474", "fayyaz@123","MD", "fayyaz", "fayyaz@gmail.com");
---
+++ week-11/hono-app/src/index.ts
@@ -0,0 +1,27 @@
+import { Hono } from 'hono'
+
+const app = new Hono();
+
+async function authMiddleWare(c:any,next:any){
+  if(c.req.header){
+    // Do Validation
+     await next()
+  }else{
+    return c.text("You dont have access");
+  }
+
+}
+app.use(authMiddleWare);
+
+
+app.post("/",authMiddleWare, async(c)=>{
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  
+  return c.text("hello Hono!");
+})
+
+
+export default app
